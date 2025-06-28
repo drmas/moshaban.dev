66fc6befe1a4e43907ebd5bc22f3b95573da1489
Mohamed Shaban <dr.m.shaban@gmail.com>
2025-06-28 21:26:13 +0200

    Updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://moshaban.co',
+  site: 'https://moshaban.dev',
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
